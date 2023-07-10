0ed91f89a4949a07fa8b96c43e1775a0c56a3e29
IlyaMelnichenko <124725419+IlyaMelnichenko@users.noreply.github.com>
2023-07-10 22:48:18 +0300

    22

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,6 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import Notiflix from "notiflix";
+
 
 const datetimePicker = document.querySelector("#datetime-picker");
 const startButton = document.querySelector("button[data-start]");
@@ -9,8 +9,11 @@ const hoursEl = document.querySelector("[data-hours]");
 const minutesEl = document.querySelector("[data-minutes]");
 const secondsEl = document.querySelector("[data-seconds]");
 
-let countdownInterval;
+let timerId = null;
 
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
 flatpickr("#datetime-picker", {
   enableTime: true,
   time_24hr: true,
@@ -24,6 +27,44 @@ flatpickr("#datetime-picker", {
       startButton.setAttribute("disabled", true);
     } else {
       startButton.removeAttribute("disabled");
+    };
+    startButton.addEventListener('click',(event)=>{
+      timerId = setInterval(()=>{
+      startButton.setAttribute("disabled", true);
+      const intervalTime =new Date();
+      const endTime= selectedDate-intervalTime;
+      if(endTime <= 0){
+        clearInterval(timerId);
+        return;
+      };
+      const {days,hours,minutes,seconds}=convertMs(endTime);
+      daysEl.textContent = addLeadingZero(days);
+      hoursEl.textContent = addLeadingZero(hours);
+      minutesEl.textContent=addLeadingZero(minutes);
+      secondsEl.textContent=addLeadingZero(seconds);
+     
+    },1000)}
+    );
+    function convertMs(ms) {
+      // Number of milliseconds per unit of time
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
+    
+      // Remaining days
+      const days = Math.floor(ms / day);
+      // Remaining hours
+      const hours = Math.floor((ms % day) / hour);
+      // Remaining minutes
+      const minutes = Math.floor(((ms % day) % hour) / minute);
+      // Remaining seconds
+      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    
+      return { days, hours, minutes, seconds };
+    };
+    function addLeadingZero(value) {
+      return value.toString().padStart(2, '0');
     }
   },
 });
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+const delayEl =document.querySelector('input[name="delay"]');
+const stepEl =document.querySelector('input[name="step"]');
+const amountEl =document.querySelector('input[name="amount"]');
+const form =document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+  return new Promise((resolve,reject)=>{
+  setTimeout(()=>{ if (shouldResolve) {
+    resolve ({position,delay});
   } else {
-    // Reject
-  }
+    reject ({position,delay});
+  }},delay);
+});
 }
+form.addEventListener('submit',(event)=>{
+  event.preventDefault();
+  
+  
+  const delay = parseInt(delayEl.value);
+  const step = parseInt(stepEl.value);
+  const amount = parseInt(amountEl.value);
+  for(let i=0;i<amount;i++){
+    createPromise(i+1,delay+i*step)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  }
+  form.reset();
+});
